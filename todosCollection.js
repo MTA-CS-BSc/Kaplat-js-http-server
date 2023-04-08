19ffa714f75a6f6a678d76a48ecce545eb8770f6
Maya Raskin <mayaraskin14@gmail.com>
2023-04-08 09:40:35 +0300

    Added size function

--- todosCollection.js
+++ todosCollection.js
@@ -1,3 +1,5 @@
+const { status } = require('./status')
+
 const todos = []
 
 function push(value) {
@@ -8,7 +10,14 @@ function find(title) {
     return todos.find(element => element.title == title)
 }
 
+function size(filter) {
+    if (filter == 'ALL')
+        return todos.length
+
+    return todos.filter(element => element.status == status[filter]).length
+}
 module.exports = {
     push,
-    find
+    find,
+    size
 }
