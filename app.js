a6cd31f238ef658adf05487d6bddb8d1a05aba7b
Maya Raskin <mayaraskin14@gmail.com>
2023-05-06 01:44:13 +0300

    Added usages of new modules

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const todoRouter = require('./api/TodoRouter')
+const { requestLogger, requestLog } = require('./modules/loggers/RequestLogger')
 
 const PORT = 8496
 const app = express()
 
-app.use('/todo', todoRouter)
+let requestsCounter = 0
+app.use((req, res, next) => {
+    req.id = ++requestsCounter
+    next()
+})
 
+app.use(requestLog)
+
+app.use('/todo', todoRouter)
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...\n`))
