0229e2c3970c9fbea66e5a3847f6b68606ad4c08
Maya Raskin <mayaraskin14@gmail.com>
2023-05-06 13:07:22 +0300

    Changed createLog to makeLog on RequestLogger

--- modules/loggers/RequestLogger.js
+++ modules/loggers/RequestLogger.js
@@ -1,14 +1,14 @@
-const { createLog, makeLogger } = require("./GenericLoggerModule")
+const { makeLog, makeLogger } = require("./GenericLoggerModule")
 
 const requestLogger = makeLogger(true, 'logs/requests.log', 'info', 'request-logger')
 
 const makeLogForRequest = (req, res, next) => {
-    createLog(requestLogger.info, `Incoming request | #${req.id} | resource: ${req.path} | HTTP Verb ${req.method}`, req.id)
+  makeLog(requestLogger.info, `Incoming request | #${req.id} | resource: ${req.path} | HTTP Verb ${req.method}`, req.id)
     
     const start = Date.now()
     res.on('finish', () => {
       const duration = Date.now() - start
-      createLog(requestLogger.debug, `request ${req.id} duration: ${duration}ms`, req.id)
+      makeLog(requestLogger.debug, `request ${req.id} duration: ${duration}ms`, req.id)
     })
 
     next()
