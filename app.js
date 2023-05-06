c7f7654fc97a7dcaaa998e949b5dcd7591a0df16
Maya Raskin <mayaraskin14@gmail.com>
2023-05-06 12:44:16 +0300

    Added usage of makeLogForRequest

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const todoRouter = require('./api/TodoRouter')
-const { requestLogger, requestLog } = require('./modules/loggers/RequestLogger')
+const { makeLogForRequest } = require('./modules/loggers/RequestLogger')
 
 const PORT = 8496
 const app = express()
 
 let requestsCounter = 0
+
 app.use((req, res, next) => {
     req.id = ++requestsCounter
     next()
 })
 
-app.use(requestLog)
+app.use(makeLogForRequest)
 
 app.use('/todo', todoRouter)
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...\n`))
